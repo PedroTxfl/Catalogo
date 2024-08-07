f39a2f85414d22140af8e4a93900331db6f1a211
Pedro Tian <pedro.tx2001@gmail.com>
2024-08-07 10:48:04 -0300

    Category extends aggregateRoot, ID turned into a value object

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/Entity.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/Entity.java
@@ -2,12 +2,12 @@ package com.fullcycle.admin.catalogo.domain;
 
 import java.util.Objects;
 
-public abstract class Entity<ID extends Indetifier> {
+public abstract class Entity<ID extends Identifier> {
 
     protected final ID id;
 
     protected Entity(final ID id) {
-        Objects.requireNonNull(id, "'id' should not be null")
+        Objects.requireNonNull(id, "'id' should not be null");
         this.id = id;
     }
 
